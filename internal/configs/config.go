b8f8e23741a1ed19255130046832d479b0303270
Ilya-SurkoV <lsurkovl@mail.ru>
2024-06-15 20:08:42 +0300

    upd work config

--- internal/configs/config.go
+++ internal/configs/config.go
@@ -6,8 +6,17 @@ import (
 )
 
 type Config struct {
-	Port      string
-	StaticURL string
+	Server struct {
+		Port      string
+		StaticURL string
+	}
+	NationalData struct {
+		Host string
+	}
+	Cache struct {
+		TimeToLifeCache int
+		Count           int
+	}
 }
 
 func New() *Config {
@@ -20,12 +29,22 @@ func New() *Config {
 func (c *Config) Print() {
 	log.Print("\n",
 		"============= CONFIG =============", "\n",
-		"PORT.................: ", c.Port, "\n",
-		"STATIC_URL...........: ", c.StaticURL, "\n",
+		"PORT.................: ", c.Server.Port, "\n",
+		"STATIC_URL...........: ", c.Server.StaticURL, "\n",
+
+		//NationalData
+		"HOST...........: ", c.NationalData.Host, "\n",
+
+		//Cahce
+		"TTL_CACHE...........: ", c.Cache.TimeToLifeCache, "\n",
+		"COUNT...........: ", c.Cache.Count, "\n",
 	)
 }
 
 func (c *Config) parseFlags() {
-	flag.StringVar(&c.Port, "http", "8080", "")
-	flag.StringVar(&c.StaticURL, "URL", "https://api.nationalize.io", "")
+	flag.StringVar(&c.Server.Port, "http", "8080", "")
+	flag.StringVar(&c.Server.StaticURL, "URL", "localhost", "")
+	flag.StringVar(&c.NationalData.Host, "Host", "https://api.nationalize.io", "")
+	flag.IntVar(&c.Cache.TimeToLifeCache, "ttlCache", 5, "")
+	flag.IntVar(&c.Cache.Count, "Count", 1000, "")
 }
